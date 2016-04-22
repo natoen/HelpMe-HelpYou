35a04177ff69350a8dcd3329005732fe0f8efb62
Sara <sgee03@gmail.com>
2016-04-22 14:58:40 -0700

    (feat) Client side connection to socketIO

---
+++ client/app/friends/chatCtrl.js
@@ -0,0 +1,29 @@
+angular
+  .module('app.chat', [])
+  .controller('ChatController', ChatController);
+
+ChatController.$inject = ['$scope', 'Socket', '$uibModal', '$log'];
+
+function ChatController($scope, Socket, $uibModal, $log) {
+  Socket.connect();
+
+  $scope.$on('$locationChangeStart', function(event) {
+    Socket.disconnect(true);
+  });
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'chat.html',
+      controller: 'SocketInstanceCtrl',
+      size: size
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+};
