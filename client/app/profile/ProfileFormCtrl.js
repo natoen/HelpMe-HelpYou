e2023dd7d7a09352dcd6202dd3460034caa69d9d
Amanda Pattridge <alpattridge@gmail.com>
2016-04-22 14:44:10 -0700

    (feat)Add controllers for profile editor

---
+++ client/app/profile/ProfileFormCtrl.js
@@ -0,0 +1,17 @@
+angular
+  .module('app.profile', [])
+  .controller('ProfileFormCtrl', function ($scope, $uibModalInstance, items) {
+
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+      $uibModalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
+    };
+  });
