19ccbe66216ff64aa296c34f47964258fb70ba86
Sara <sgee03@gmail.com>
2016-04-22 15:03:41 -0700

    (feat)Creates new instance of chatCtrl

---
+++ client/app/friends/chatInstanceCtrl.js
@@ -0,0 +1,16 @@
+angular
+  .module('app.chat')
+  .controller('ChatInstanceCtrl', ChatInstanceCtrl);
+  
+  ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance'];
+  
+  function ChatInstanceCtrl ($scope, $uibModalInstance) {
+
+  $scope.ok = function () {
+    $uibModalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+};
