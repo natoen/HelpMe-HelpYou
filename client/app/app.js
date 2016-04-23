8f34c08e490496be92785f078738c38c59b40f90
fswiecki <frances.swiecki@gmail.com>
2016-04-23 11:41:59 -0400

    (feat) insert edit-in-place directive
    Not yet functional

--- client/app/app.js
+++ client/app/app.js
@@ -20,6 +20,7 @@ angular
     'app.chat'
   ])
   .config(config)
+  .directive(editInPlace)
   .run(authCheck)
   .factory('Socket', ['socketFactory', function(socketFactory) {
     return socketFactory();
@@ -150,3 +151,42 @@ function authCheck($rootScope, auth, store, jwtHelper, $location, $state) {
     }
   });
 }
+
+editInPlace.$inject = ['$scope', 'element', 'attrs'];
+
+function editInPlace($scope, element, attrs) {
+  return{
+    restrict: 'E',
+    scope: { value: '=' },
+    template: '<span ng-click="edit()" ng-bind="value"></span><input ng-model="value"></input>',
+    link: function ( $scope, element, attrs ) {
+      // Let's get a reference to the input element, as we'll want to reference it.
+      var inputElement = angular.element( element.children()[1] );
+      
+      // This directive should have a set class so we can style it.
+      // element.addClass( 'edit-in-place' );
+      
+      // Initially, we're not editing.
+      $scope.editing = false;
+      
+      // ng-click handler to activate edit-in-place
+      $scope.edit = function () {
+        $scope.editing = true;
+        
+        // We control display through a class on the directive itself. See the CSS.
+        element.addClass( 'active' );
+        
+        // And we must focus the element. 
+        // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function, 
+        // we have to reference the first element in the array.
+        inputElement[0].focus();
+      };
+      
+      // When we leave the input, we're done editing.
+      inputElement.prop( 'onblur', function() {
+        $scope.editing = false;
+        element.removeClass( 'active' );
+      });
+    }
+  };
+};
