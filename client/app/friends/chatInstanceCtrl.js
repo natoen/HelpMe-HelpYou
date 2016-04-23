7a5f0ca7b41e768a18d447f6da96a18352ba1d61
Sara <sgee03@gmail.com>
2016-04-22 23:12:09 -0700

    (feat) Emit and on listener for messages inputted

--- client/app/friends/chatInstanceCtrl.js
+++ client/app/friends/chatInstanceCtrl.js
@@ -2,29 +2,29 @@ angular
   .module('app.chat')
   .controller('ChatInstanceCtrl', ChatInstanceCtrl);
   
-ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance', 'Socket'];
+ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance', 'Socket', 'auth', 'Profile'];
   
-function ChatInstanceCtrl($scope, $uibModalInstance, Socket) {
-
+function ChatInstanceCtrl($scope, $uibModalInstance, Socket, auth, Profile) {
   $scope.messages = [];
 
   $scope.sendMessage = function(message) {
-    console.log(message);
     if (message != false) {
-      console.log('YOUGOTINSIDE!');
-      Socket.emit('message', {message: message});
-      $scope.messages.push(message);
-    console.log($scope.messages)
+      Socket.emit('message', {username: $scope.profile.nickname, message: message});
     }
-
     $scope.privateMessage = '';
   };
+
+  Socket.on('message', function(data) {
+    $scope.messages.push(data);
+    console.log($scope.messages)
+  });
+
+  auth.profilePromise.then(function(profile) {
+    $scope.profile = profile;
+  });
 
   $scope.ok = function() {
     $uibModalInstance.close();
   };
 
-  $scope.cancel = function() {
-    $uibModalInstance.dismiss('cancel');
-  };
-}
+};
