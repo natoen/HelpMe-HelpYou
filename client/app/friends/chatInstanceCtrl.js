3eb872050adb6901bbc1c9328aa90212da052362
Sara <sgee03@gmail.com>
2016-04-22 18:00:25 -0700

    (feat) Show user messages on panel

--- client/app/friends/chatInstanceCtrl.js
+++ client/app/friends/chatInstanceCtrl.js
@@ -2,9 +2,23 @@ angular
   .module('app.chat')
   .controller('ChatInstanceCtrl', ChatInstanceCtrl);
   
-  ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance'];
+ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance', 'Socket'];
   
-  function ChatInstanceCtrl($scope, $uibModalInstance) {
+function ChatInstanceCtrl($scope, $uibModalInstance, Socket) {
+
+  $scope.messages = [];
+
+  $scope.sendMessage = function(message) {
+    console.log(message);
+    if (message != false) {
+      console.log('YOUGOTINSIDE!');
+      Socket.emit('message', {message: message});
+      $scope.messages.push(message);
+    console.log($scope.messages)
+    }
+
+    $scope.privateMessage = '';
+  };
 
   $scope.ok = function() {
     $uibModalInstance.close();
@@ -13,4 +27,4 @@ angular
   $scope.cancel = function() {
     $uibModalInstance.dismiss('cancel');
   };
-};
+}
