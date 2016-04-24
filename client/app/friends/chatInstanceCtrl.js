37c7777f58a07a3ebcb657e1468208d7abac5d12
Sara <sgee03@gmail.com>
2016-04-23 18:10:11 -0700

    (feat) Client listens on messsages and filters to only 1:1 chats

--- client/app/friends/chatInstanceCtrl.js
+++ client/app/friends/chatInstanceCtrl.js
@@ -2,21 +2,24 @@ angular
   .module('app.chat')
   .controller('ChatInstanceCtrl', ChatInstanceCtrl);
   
-ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance', 'Socket', 'auth', 'Profile'];
+ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance', 'Socket', 'auth', 'Profile', 'current'];
   
-function ChatInstanceCtrl($scope, $uibModalInstance, Socket, auth, Profile) {
+function ChatInstanceCtrl($scope, $uibModalInstance, Socket, auth, Profile, current) {
   $scope.messages = [];
+  $scope.chatFriend = current;
 
   $scope.sendMessage = function(message) {
     if (message != false) {
-      Socket.emit('message', {username: $scope.profile.nickname, message: message});
+      Socket.emit('message', {username: $scope.profile.nickname, friend: $scope.chatFriend, message: message});
     }
     $scope.privateMessage = '';
   };
 
   Socket.on('message', function(data) {
-    $scope.messages.push(data);
-    console.log($scope.messages)
+    if (($scope.chatFriend === data.friend || $scope.chatFriend === data.username)
+      && ($scope.profile.nickname === data.username || $scope.profile.nickname === data.friend)) {
+      $scope.messages.push(data);
+    };
   });
 
   auth.profilePromise.then(function(profile) {
