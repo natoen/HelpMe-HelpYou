c6e2091d3dd28df7e2654bb7ebbdfa914f163340
natoen <hgmatsuda@gmail.com>
2016-04-25 14:54:15 +0900

    (feat) Load old messages when initialize

--- client/app/friends/chatInstanceCtrl.js
+++ client/app/friends/chatInstanceCtrl.js
@@ -7,6 +7,7 @@ ChatInstanceCtrl.$inject = ['$scope', '$uibModalInstance', 'Socket', 'auth', 'Pr
 function ChatInstanceCtrl($scope, $uibModalInstance, Socket, auth, Profile, current) {
   $scope.messages = [];
   $scope.chatFriend = current;
+  $scope.msgNotLoaded = true;
 
   $scope.sendMessage = function(message) {
     if (message != false) {
@@ -16,18 +17,21 @@ function ChatInstanceCtrl($scope, $uibModalInstance, Socket, auth, Profile, curr
   };
 
   Socket.on('message', function(data) {
-    if (($scope.chatFriend === data.friend || $scope.chatFriend === data.username)
-      && ($scope.profile.nickname === data.username || $scope.profile.nickname === data.friend)) {
+    if (($scope.chatFriend === data.friend || $scope.chatFriend === data.username) &&
+        ($scope.profile.nickname === data.username || $scope.profile.nickname === data.friend)) {
       $scope.messages.push(data);
-    };
+    }
   });
+
+
 
   auth.profilePromise.then(function(profile) {
     $scope.profile = profile;
+    Socket.emit('loadMessages', { username: $scope.profile.nickname, friend: $scope.chatFriend});
   });
 
   $scope.ok = function() {
     $uibModalInstance.close();
   };
 
-};
+}
