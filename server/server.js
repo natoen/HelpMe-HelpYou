7e03cfd11504426e53ef0275f9733ed0f88a41fd
natoen <hgmatsuda@gmail.com>
2016-04-25 14:48:45 +0900

    (feat) Add load old messages function

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var messageSchema = require('./messages/messageSchema.js');
 
 var app = express();
 var server = require('http').Server(app);
@@ -10,8 +11,35 @@ io.on('connection', function (socket) {
   console.log('A user has connected!');
 
   socket.on('message', function(data) {
-    io.emit('message', {username: data.username, friend: data.friend, message: data.message});
-  })
+    var newMessage = new messageSchema({username: data.username, friend: data.friend, message: data.message});
+
+    newMessage.save(function(err) {
+      if (err) { throw err; }
+      io.emit('message', {username: data.username, friend: data.friend, message: data.message});
+    });
+  });
+
+  socket.on('loadMessages', function(data) {
+    messageSchema.find({ $or:
+        [{username: data.username, friend: data.friend},
+         {username: data.friend, friend: data.username}]
+      })
+      .limit(20)
+      .sort('+createdAt')
+      .exec(function(err, data) {
+        if (data) {
+          for (var i = 0; i < data.length; i++) {
+            delete data[i].createdAt;
+            delete data[i].__v;
+            delete data[i]._id;
+            io.emit('loadMessage', data[i]);
+          }
+        }
+
+        io.emit('msgNotLoaded', false);
+      });
+
+  });
 
   socket.on('disconnect', function() {
     console.log('A user has disconnected...');
